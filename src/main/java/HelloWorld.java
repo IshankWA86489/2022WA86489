bf7460c15baea96401a89c2bb5cea57be7aec867
IshankWA86489 <2022wa86489@wilp.bits-pilani.ac.in>
2026-09-22 02:53:32 +0530

    Modified HelloWorld source code

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from BITS ID 2022WA86489");
+        System.out.println("Continuous Integration using Jenkins");
     }
 }
